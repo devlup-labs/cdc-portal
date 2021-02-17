520e8f01196948835b5847e86e1b96d9e131d898
Mohan Chhabaria <chhabaria.1@iitj.ac.in>
2021-02-18 01:40:31 +0530

    include company into main urls

--- backend/cdc_portal/urls.py
+++ backend/cdc_portal/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/student/', include("student.urls"))
+    path('api/student/', include("student.urls")),
+    path('api/company/', include('company.urls'))
 ]
 
 if settings.DEBUG:
